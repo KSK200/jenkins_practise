33e38870a1d134c0e8837de8dc53d9edd0588ba6
karthik Kavathekar <karthik_kavathekar@thbs.india.com>
2024-01-04 10:05:01 +0530

    file3.java and file3.class added

--- file3.java
+++ file3.java
@@ -1,7 +1,10 @@
 class file3{
 public static void main(String args[]){
 	
-	System.out.println("Hi.....");
-
- 	}
+	System.out.println("Hi Karthik Here.....");
+		
+		for(int i=1;i<=10;i++){
+			System.out.println(i);
+		}
+	}	
 }
