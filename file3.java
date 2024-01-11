1699529db068c6ab2eb4f387f977cf29adbbe187
Karthik Kavathekar <96935395+KSK200@users.noreply.github.com>
2024-01-11 12:39:43 +0530

    Update file3.java

--- file3.java
+++ file3.java
@@ -1,7 +1,11 @@
 class file3{
 public static void main(String args[]){
 	
-	System.out.println("Hi.....");
+	System.out.println("Hi This Is Karthik");
+	for(int i=0;i<10;i++){
+
+		System.out.println("i="+i);
+	}
 
  	}
 }
